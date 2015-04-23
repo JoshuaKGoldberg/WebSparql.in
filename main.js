4d07228ca887ada83ca26c2b104a0dd18c658a79
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-04-22 22:41:39 -0400

    Added main landing page

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 var express = require("express"),
-    app = express();
+    url = require("url"),
+    app = express(),
+    indexOptions = {
+        "root": __dirname + "/static/",
+        "dotfiles": "deny",
+        "headers": {
+            "x-timestamp": Date.now(),
+            "x-sent": true
+        }
+    };
 
 app.set("port", (process.env.PORT || 5000));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (request, response) {
-    response.set('Content-Type', 'text/html');
-    response.write("Hello world.");
-    response.end();
+// Redirect everything to the /static/ dir (because of indexOptions)
+app.get("*", function (request, response) {
+    response.sendFile(request.url, indexOptions, function () {
+        response.end();
+    });
 });
+
+//// 404 page: index.html
+//app.error(function (error, request, result, next) {
+//    response.sendFile("404.html", indexOptions, function () {
+//        response.end();
+//    });
+//});
 
 app.listen(app.get("port"), function () {
     console.log("Server running at " + app.get("port"));
